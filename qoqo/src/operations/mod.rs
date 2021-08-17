f1315a14217ff620ccacb61866936736a39ccc96
kbarkhqs <65908581+kbarkhqs@users.noreply.github.com>
2021-08-17 17:08:38 +0200

    Update mod.rs

--- qoqo/src/operations/mod.rs
+++ qoqo/src/operations/mod.rs
@@ -173,6 +173,9 @@ pub fn operations(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<PMInteractionWrapper>()?;
     m.add_class::<ComplexPMInteractionWrapper>()?;
     m.add_class::<PhaseShiftedControlledZWrapper>()?;
+    m.add_class::<PhaseShiftState0Wrapper>()?;
+    m.add_class::<PhaseShiftState1Wrapper>()?;
+
 
     Ok(())
 }
