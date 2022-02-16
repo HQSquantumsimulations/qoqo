03dc9ec24efc6978d0fcf8552b86b852a26a6843
Marina Walt <marina.walt@quantumsimulations.com>
2022-02-16 17:56:45 +0000

    trying to fix math mode issues in docstrings

--- roqoqo/src/operations/single_qubit_gate_operations.rs
+++ roqoqo/src/operations/single_qubit_gate_operations.rs
@@ -164,7 +164,8 @@ impl OperateSingleQubitGate for SingleQubitGate {
 /// U = \begin{pmatrix}
 /// \cos(\frac{\theta}{2}) & 0 \\\\
 /// 0 & \cos(\frac{\theta}{2})
-/// \end{pmatrix} + \begin{pmatrix}
+/// \end{pmatrix}
+/// + \begin{pmatrix}
 ///  -i \sin(\frac{\theta}{2}) & 0 \\\\
 /// 0 & i \sin(\frac{\theta}{2})
 /// \end{pmatrix}
