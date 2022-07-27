ca1d39fa3252745a91004396876456480d781b48
Nicolas Vogt <45093608+nfwvogt@users.noreply.github.com>
2022-07-27 08:55:48 +0200

    Add involved classical (#306)
    * WIP: async compatability
    
    * fix save error
    
    * removed double import
    
    * implemented bit input
    
    * First working version of involved_classical
    
    * updated test coverage
    
    * updated test coverage
    
    * updated test coverage
    
    * updated test coverage
    
    * updated test coverage

--- roqoqo/src/operations/measurement_operations.rs
+++ roqoqo/src/operations/measurement_operations.rs
@@ -21,6 +21,8 @@ use crate::operations::{
 };
 use crate::Circuit;
 
+use super::InvolvedClassical;
+
 /// Measurement gate operation.
 ///
 /// This Operation acts on one qubit writing the result of the measurement into a readout.
@@ -50,6 +52,20 @@ pub struct MeasureQubit {
     readout: String,
     /// The index in the readout the result is saved to.
     readout_index: usize,
+}
+
+impl InvolveQubits for MeasureQubit {
+    fn involved_qubits(&self) -> InvolvedQubits {
+        let mut a: HashSet<usize> = HashSet::new();
+        a.insert(self.qubit);
+        InvolvedQubits::Set(a)
+    }
+
+    fn involved_classical(&self) -> super::InvolvedClassical {
+        let mut a: HashSet<(String, usize)> = HashSet::new();
+        a.insert((self.readout.clone(), self.readout_index));
+        InvolvedClassical::Set(a)
+    }
 }
 
 #[allow(non_upper_case_globals)]
@@ -101,6 +117,10 @@ impl InvolveQubits for PragmaGetStateVector {
     /// Lists all involved qubits (here: All).
     fn involved_qubits(&self) -> InvolvedQubits {
         InvolvedQubits::All
+    }
+
+    fn involved_classical(&self) -> InvolvedClassical {
+        InvolvedClassical::All(self.readout.clone())
     }
 }
 
@@ -156,6 +176,10 @@ impl InvolveQubits for PragmaGetDensityMatrix {
     /// Lists all involved qubits (here, all).
     fn involved_qubits(&self) -> InvolvedQubits {
         InvolvedQubits::All
+    }
+
+    fn involved_classical(&self) -> InvolvedClassical {
+        InvolvedClassical::All(self.readout.clone())
     }
 }
 
@@ -214,6 +238,10 @@ impl InvolveQubits for PragmaGetOccupationProbability {
     /// Lists all involved qubits (here, all).
     fn involved_qubits(&self) -> InvolvedQubits {
         InvolvedQubits::All
+    }
+
+    fn involved_classical(&self) -> InvolvedClassical {
+        InvolvedClassical::All(self.readout.clone())
     }
 }
 
@@ -290,6 +318,10 @@ impl InvolveQubits for PragmaGetPauliProduct {
         }
         InvolvedQubits::Set(new_hash_set)
     }
+
+    fn involved_classical(&self) -> InvolvedClassical {
+        InvolvedClassical::All(self.readout.clone())
+    }
 }
 
 /// This PRAGMA measurement operation returns a measurement record for N repeated measurements.
@@ -354,5 +386,16 @@ impl InvolveQubits for PragmaRepeatedMeasurement {
     /// Lists all involved qubits (here, all).
     fn involved_qubits(&self) -> InvolvedQubits {
         InvolvedQubits::All
+    }
+
+    fn involved_classical(&self) -> InvolvedClassical {
+        match &self.qubit_mapping {
+            None => InvolvedClassical::AllQubits(self.readout.clone()),
+            Some(x) => {
+                let new_set: HashSet<(String, usize)> =
+                    x.values().map(|v| (self.readout.clone(), *v)).collect();
+                InvolvedClassical::Set(new_set)
+            }
+        }
     }
 }
