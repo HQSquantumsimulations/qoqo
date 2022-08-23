2c9827473c1ec9e8d2bde92271fb926e4d599183
Matteo Lodi <108724576+mlodi-hqs@users.noreply.github.com>
2022-08-23 11:38:21 +0000

    Add circuit dag qoqo interface (#334)
    * Initial commit
    
    * Basic data structure
    
    * Test file added
    
    * Added petgraph dependency
    
    * First test, fixed lib and toml
    
    * Ci update (#304)
    
    * WIP: async compatability
    
    * fix save error
    
    * removed double import
    
    * updated ci to run for 1.1.0
    
    * Added getters. First test.
    
    * Developing add_to_back()
    
    * First_all, last_all still WIP
    
    * Finished first_all last_all implementation
    
    * Removed import
    
    * More development for All case
    
    * For loops WIP
    
    * Reorder, better for loops
    
    * All main calls present
    
    * Added checks, node_count test
    
    * Comments for TODO, test correction
    
    * last_parallel_block test
    
    * Fixed tests, added last_op_inv_qbts test
    
    * Improved no_inv_qbts test
    
    * add_to_front(), missing one subfunction
    
    * Added getters, finished add_to_front()
    
    * Populating tests with front.
    
    * Added front and back Option<NodeIndex> returns.
    
    * Corrected adding from set behaviour. More tests.
    
    * Implemented InvolvedClassical, untested.
    
    * Popolating InvolvedClassical tests
    
    * Extended tests, implmented new_from_circuit
    
    * More comments, added test for new_from_circuit
    
    * Test reorder
    
    * Major reordering, cleaner solutions
    
    * Restructured classical handling, uncomplete tests
    
    * Quick fix, InvolvedClassic complete (?)
    
    * Fixed commuting_operations.
    
    * Implemented From<Circuit>
    
    * NodeIndex now usize
    
    * Cleaned, test pragma conditional
    
    * clippy stuff
    
    * First qoqo CircuitDag commit
    
    * Added CircuitDag getter
    
    * All main pub fn implemented
    
    * Created CircuitDagWrapper test
    
    * Beginning tests
    
    * Extended tests
    
    * fixes
    
    * Extended tests
    
    * circuitdag to feature
    
    * Implemented and tested From<CircuitDag>
    
    * Fix
    
    * Added CircuitDagWrapper to qoqo/lib.rs
    
    * Added copy, richcmp. Tested copy.
    
    * Tested richcmp
    
    * Implemented and tested from_circuit
    
    * Implemented and tested to_circuit
    
    * Corrections from PR
    
    * Correction
    
    * Implemented execution_blocked
    
    * Still WIP
    
    * Better handling of InvolvedQubits::All case
    
    * new_front_layer and execution_blocked implemented
    
    * Finished impl + testing new_front_layer
    
    * Before new struct
    
    * fmt
    
    * another fmt
    
    * Initial ParallelBlocks structure
    
    * ParallelBlock Iterator (untested)
    
    * tested ParallelBlocks
    
    * fmt
    
    * qoqo execution_blocked and initial new_front_layer
    
    * Finished qoqo interfaces
    
    * Tested qoqo execution_blocked
    
    * Tested qoqo new_front_layer
    
    * Tested qoqo parallel_blocks + roqoqo modifications
    
    * fmt
    
    * comments fix
    
    * Missed one test from merging
    
    * Fixed qoqo test_parallel_blocks
    
    * Improved PartialEq + test_partial_eq
    
    * fmt
    
    * clippy
    
    * fix
    
    * fixes and comments
    
    * Implemented roqoqo blocking_predecessor + test
    
    * Implemented qoqo blocking_predecessors + test
    
    * Added serialization methods
    
    * FIxed first_ last_parallel_blocks bug
    
    * fmt
    
    * Added and tested qoqo getters
    
    * Corrections from PR
    
    * Changed interface for ParallelBlocks Iterator
    
    * Added pyo3 signatures
    
    * fmt
    
    Co-authored-by: Nicolas Vogt <45093608+nfwvogt@users.noreply.github.com>

--- roqoqo/src/circuitdag.rs
+++ roqoqo/src/circuitdag.rs
@@ -14,13 +14,20 @@ use std::collections::{HashMap, HashSet};
 
 use crate::operations::*;
 use crate::Circuit;
+use crate::{RoqoqoError, RoqoqoVersion};
 
 use petgraph::adj::NodeIndex;
+use petgraph::algo;
+use petgraph::algo::toposort;
 use petgraph::graph::Graph;
+use petgraph::visit::Dfs;
 use petgraph::Directed;
+use petgraph::Direction::{Incoming, Outgoing};
 
 /// Represents the Direct Acyclic Graph (DAG) of a Circuit.
-#[derive(Debug)]
+///
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "serialize", derive(serde::Serialize, serde::Deserialize))]
 pub struct CircuitDag {
     pub(crate) graph: Graph<Operation, (), Directed, usize>,
     pub(crate) commuting_operations: Vec<NodeIndex<usize>>,
@@ -32,6 +39,28 @@ pub struct CircuitDag {
     pub(crate) last_operation_involving_qubit: HashMap<usize, NodeIndex<usize>>,
     pub(crate) first_operation_involving_classical: HashMap<(String, usize), NodeIndex<usize>>,
     pub(crate) last_operation_involving_classical: HashMap<(String, usize), NodeIndex<usize>>,
+    _roqoqo_version: RoqoqoVersion,
+}
+
+/// Iterator over all possible parallel executable blocks of a Circuit.
+///
+#[derive(Debug)]
+pub struct ParallelBlocks<'a> {
+    dag: &'a CircuitDag,
+    parallel_block: Vec<NodeIndex<usize>>,
+    already_executed: Vec<NodeIndex<usize>>,
+}
+
+impl PartialEq for CircuitDag {
+    fn eq(&self, other: &Self) -> bool {
+        let nodes = |a: &Operation, b: &Operation| a.eq(b);
+        let edges = |_: &(), _: &()| true;
+        algo::is_isomorphic_matching(&self.graph, &other.graph, nodes, edges)
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        return !self.eq(other);
+    }
 }
 
 impl CircuitDag {
@@ -50,14 +79,15 @@ impl CircuitDag {
             first_operation_involving_classical: HashMap::<(String, usize), NodeIndex<usize>>::new(
             ),
             last_operation_involving_classical: HashMap::<(String, usize), NodeIndex<usize>>::new(),
+            _roqoqo_version: RoqoqoVersion,
         }
     }
 
-    /// Adds an operation to the back of the CircuitDag if necessary.
+    /// Adds an operation to the back of the CircuitDag, if necessary.
     ///
     /// # Arguments
     ///
-    /// * 'operation' - The Operation to add to the end of the CircuitDag.
+    /// * 'operation' - The Operation to add to the back of the CircuitDag.
     ///
     /// # Returns
     ///
@@ -101,6 +131,14 @@ impl CircuitDag {
             for qubit in x {
                 self.update_from_qubit_back(node, qubit);
             }
+            if self
+                .graph
+                .neighbors_directed(node.into(), Incoming)
+                .next()
+                .is_none()
+            {
+                self.first_parallel_block.insert(node);
+            }
         } else if let InvolvedQubits::All = node_involved_qubits {
             self.update_from_all_operation_back(node);
         }
@@ -117,23 +155,25 @@ impl CircuitDag {
         // Update last_operation_involving qubit and last_parallel_block
         //  depending on current structure
         if let Some(&i) = self.last_operation_involving_qubit.get(&qubit) {
-            self.graph.add_edge(i.into(), node.into(), ());
+            self.graph.update_edge(i.into(), node.into(), ());
             self.last_parallel_block.remove(&i);
+        } else if self.last_all.is_some() {
+            self.graph
+                .update_edge(self.last_all.unwrap().into(), node.into(), ());
+            self.last_parallel_block
+                .remove(&self.last_all.unwrap().into());
         }
         let qubit_presence = self.last_operation_involving_qubit.insert(qubit, node);
         self.last_parallel_block.insert(node);
 
         // Update the first layer in case the qubit has never been seen before
         if qubit_presence.is_none() {
-            // Update first_operation_involving_qubit and first_parallel_block
-            //  depending on last_all
+            // Update first_operation_involving_qubit depending on last_all
             if self.last_all.is_none() {
                 self.first_operation_involving_qubit.insert(qubit, node);
-                self.first_parallel_block.insert(node);
             } else {
                 self.first_operation_involving_qubit
                     .insert(qubit, self.last_all.unwrap());
-                self.first_parallel_block.insert(self.last_all.unwrap());
             }
         }
     }
@@ -226,6 +266,14 @@ impl CircuitDag {
             for qubit in x {
                 self.update_from_qubit_front(node, qubit);
             }
+            if self
+                .graph
+                .neighbors_directed(node.into(), Outgoing)
+                .next()
+                .is_none()
+            {
+                self.last_parallel_block.insert(node);
+            }
         } else if let InvolvedQubits::All = node_involved_qubits {
             self.update_from_all_operation_front(node);
         }
@@ -242,23 +290,25 @@ impl CircuitDag {
         // Update first_operation_involving qubit and first_parallel_block
         //  depending on current structure
         if let Some(&i) = self.first_operation_involving_qubit.get(&qubit) {
-            self.graph.add_edge(node.into(), i.into(), ());
+            self.graph.update_edge(node.into(), i.into(), ());
             self.first_parallel_block.remove(&i);
+        } else if self.first_all.is_some() {
+            self.graph
+                .update_edge(node.into(), self.first_all.unwrap().into(), ());
+            self.first_parallel_block
+                .remove(&self.first_all.unwrap().into());
         }
         let qubit_presence = self.first_operation_involving_qubit.insert(qubit, node);
         self.first_parallel_block.insert(node);
 
         // Update the last layer in case the qubit has never been seen before
         if qubit_presence.is_none() {
-            // Update last_operation_involving_qubit and last_parallel_block
-            //  depending on first_all
+            // Update last_operation_involving_qubit depending on first_all
             if self.first_all.is_none() {
                 self.last_operation_involving_qubit.insert(qubit, node);
-                self.last_parallel_block.insert(node);
             } else {
                 self.last_operation_involving_qubit
                     .insert(qubit, self.first_all.unwrap());
-                self.last_parallel_block.insert(self.first_all.unwrap());
             }
         }
     }
@@ -430,9 +480,9 @@ impl CircuitDag {
                 }
             }
             InvolvedClassical::All(x) | InvolvedClassical::AllQubits(x) => {
-                // Cycle first_operation_involving_classical
                 let mut temp_map: HashMap<(String, usize), NodeIndex<usize>> =
                     HashMap::with_capacity(self.first_operation_involving_classical.capacity());
+                // Cycle first_operation_involving_classical
                 for (name, readout) in self.first_operation_involving_classical.keys() {
                     // If the classical register's name in InvolvedClassical::All or ::AllQubits
                     //  is present in first_operation_involving_classical, insert the node
@@ -448,13 +498,156 @@ impl CircuitDag {
         }
     }
 
+    /// Checks if executing an Operation is blocked by any previous not-yet executed Operation.
+    /// The scope of the check is the whole graph.
+    ///
+    /// # Arguments:
+    ///
+    /// * `already_executed` - Slice of NodeIndices of Nodes that have already been executed in the Circuit.
+    /// * `to_be_executed` - NodeIndex of the Operation that should be executed next.
+    ///
+    /// # Returns:
+    ///
+    /// * `Vec<NodeIndex<usize>>` - Vector containing the sorted blocking elements.
+    pub fn execution_blocked(
+        &self,
+        already_executed: &[NodeIndex<usize>],
+        to_be_executed: &NodeIndex<usize>,
+    ) -> Vec<NodeIndex<usize>> {
+        let mut blocking_elements: Vec<NodeIndex<usize>> = vec![];
+        let mut rev_graph: Graph<Operation, (), Directed, usize> = self.graph.clone();
+        rev_graph.reverse();
+        let mut dfs = Dfs::new(&rev_graph, (*to_be_executed).into());
+        dfs.next(&rev_graph);
+
+        // Perform a DFS on the reversed graph starting from to_be_executed,
+        //  pushing on blocking_elements the nodes not contained in already_executed
+        while let Some(nxt) = dfs.next(&rev_graph) {
+            if !already_executed.contains(&nxt.index()) {
+                blocking_elements.push(nxt.index());
+            }
+        }
+
+        blocking_elements.sort_unstable();
+        blocking_elements
+    }
+
+    /// Checks which of the direct predecessors of an Operation in the CircuitDag blocks the execution.
+    ///
+    /// # Warning:
+    ///
+    /// This method can only be used to determine if an operation can be executed when `already_executed` is consistent.
+    /// When the list `already_executed` is inconsistent (an operation is reported as executed that could not have been executed yet)
+    /// this method returning an empty vector does not imply that the `to_be_executed` operation can be executed.
+    ///
+    /// # Arguments:
+    ///
+    /// * `already_executed` - Slice of NodeIndices of Nodes that have already been executed in the Circuit.
+    /// * `to_be_executed` - NodeIndex of the Operation that should be executed next.
+    pub fn blocking_predecessors(
+        &self,
+        already_executed: &[NodeIndex<usize>],
+        to_be_executed: &NodeIndex<usize>,
+    ) -> Vec<NodeIndex<usize>> {
+        let mut blocking_elements: Vec<NodeIndex<usize>> = vec![];
+        let mut neighbor_iter = self
+            .graph
+            .neighbors_directed((*to_be_executed).into(), Incoming);
+
+        while let Some(nxt) = neighbor_iter.next() {
+            if !already_executed.contains(&nxt.index()) {
+                blocking_elements.push(nxt.index());
+            }
+        }
+
+        blocking_elements.sort_unstable();
+        blocking_elements
+    }
+
+    /// Returns a new front layer after executing an operation from the current front layer.
+    /// A front layer is a set of Operations that are ready to be executed, because all required
+    /// predecessors in the graph have already been executed.
+    ///
+    /// Returns an error if operation to be executed is not in the current front layer.
+    ///
+    /// # Arguments:
+    ///
+    /// * `already_executed` - Slice of NodeIndices of Nodes that have already been executed in the Circuit.
+    /// * `current_front_layer` - Slice of NodeIndices in the current front layer ready to be executed if physically possible.
+    /// * `to_be_executed` - NodeIndex of the operation that should be executed next.
+    pub fn new_front_layer(
+        &self,
+        already_executed: &[NodeIndex<usize>],
+        current_front_layer: &[NodeIndex<usize>],
+        to_be_executed: &NodeIndex<usize>,
+    ) -> Result<Vec<NodeIndex<usize>>, RoqoqoError> {
+        // Raise an error if the node to be executed is not in the current
+        //  front layer
+        if !current_front_layer.contains(to_be_executed) {
+            Err(RoqoqoError::GenericError {
+                msg: "The Operation to be executed is not in the current front layer.".to_string(),
+            })
+        } else {
+            let mut current_front_layer = current_front_layer.to_vec();
+            let mut added: bool = false;
+            let mut neighbor_iter = self
+                .graph
+                .neighbors_directed((*to_be_executed).into(), Outgoing);
+
+            // Boolean needed for end of graph case
+            let mut check_emptiness = neighbor_iter.clone();
+            let mut empty: bool = false;
+            if let None = check_emptiness.next() {
+                empty = true;
+            }
+
+            // Already_executed vector extension for execution_blocked() compatibility
+            let mut extended_a_e: Vec<NodeIndex<usize>> = Vec::from(already_executed);
+            extended_a_e.push(*to_be_executed);
+            // Cycle through each neighbor of to_be_executed
+            while let Some(nxt) = neighbor_iter.next() {
+                // Push the neighbor into the current front layer if it is not execution blocked
+                //  by any other node
+                if self
+                    .execution_blocked(&extended_a_e, &nxt.index())
+                    .is_empty()
+                {
+                    current_front_layer.push(nxt.index());
+                    added = true;
+                }
+            }
+            // Remove to_be_executed from the current front layer
+            if added || empty {
+                current_front_layer.remove(
+                    current_front_layer
+                        .iter()
+                        .position(|&x| x == *to_be_executed)
+                        .unwrap(),
+                );
+            }
+            Ok(current_front_layer)
+        }
+    }
+
+    /// Returns an iterator over the possible parallel blocks in circuit that can be executed simultaneously
+    ///
+    /// Returns an Iterator over Vectors of references to the NodeIndices in the parallel block as well
+    /// as references to the Operation in the blocks
+    pub fn parallel_blocks<'a>(&'a self) -> ParallelBlocks<'a> {
+        ParallelBlocks {
+            dag: self,
+            parallel_block: Vec::<NodeIndex<usize>>::new(),
+            already_executed: Vec::<NodeIndex<usize>>::new(),
+        }
+    }
+
     /// Returns a reference to the vector of commuting operations in CircuitDag.
     ///
     pub fn commuting_operations(&self) -> &Vec<usize> {
         &self.commuting_operations
     }
 
-    /// Returns a reference to the HasSet containing the nodes in the first parallel block.
+    /// Returns a reference to the HashSet containing the nodes in the first parallel block.
     ///
     pub fn first_parallel_block(&self) -> &HashSet<usize> {
         &self.first_parallel_block
@@ -495,6 +688,13 @@ impl CircuitDag {
     pub fn last_operation_involving_classical(&self) -> &HashMap<(String, usize), usize> {
         &self.last_operation_involving_classical
     }
+
+    /// Given a NodeIndex, returns a reference to the Operation contained in the node of
+    /// the CircuitDag.
+    ///
+    pub fn get(&self, node: NodeIndex<usize>) -> Option<&Operation> {
+        self.graph.node_weight(node.into())
+    }
 }
 
 /// Creates a new CircuitDag from a given Circuit.
@@ -516,6 +716,7 @@ impl From<Circuit> for CircuitDag {
             first_operation_involving_classical: HashMap::<(String, usize), NodeIndex<usize>>::new(
             ),
             last_operation_involving_classical: HashMap::<(String, usize), NodeIndex<usize>>::new(),
+            _roqoqo_version: RoqoqoVersion,
         };
 
         for operation in circuit.operations() {
@@ -523,6 +724,76 @@ impl From<Circuit> for CircuitDag {
         }
 
         new_dag
+    }
+}
+
+/// Creates a new Circuit from a given CircuitDag.
+///
+impl From<CircuitDag> for Circuit {
+    fn from(dag: CircuitDag) -> Circuit {
+        let mut circuit: Circuit = Circuit::new();
+
+        for com_op in dag.commuting_operations() {
+            let op = dag.get(*com_op).unwrap();
+            circuit.add_operation(op.clone());
+        }
+
+        match toposort(&dag.graph, None) {
+            Ok(order) => {
+                for i in order {
+                    circuit.add_operation(dag.graph.node_weight(i).unwrap().clone());
+                }
+            }
+            Err(_) => {
+                println!("Error: graph not acyclic");
+            }
+        }
+
+        circuit
+    }
+}
+
+impl<'a> Iterator for ParallelBlocks<'a> {
+    type Item = Vec<NodeIndex<usize>>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // First case
+        if self.parallel_block.is_empty() && self.already_executed.is_empty() {
+            for node in &self.dag.first_parallel_block {
+                self.parallel_block.push(*node);
+            }
+            return Some(self.parallel_block.clone());
+        }
+
+        // Populate already_executed with current parallel_block
+        for node in &self.parallel_block {
+            self.already_executed.push(*node);
+        }
+
+        let mut new_parallel_block: Vec<NodeIndex<usize>> = Vec::new();
+        // Cycle through the current parallel block
+        for node in &self.parallel_block {
+            let mut neighbor_iter = self.dag.graph.neighbors_directed((*node).into(), Outgoing);
+            // Cycle through its neighbors
+            while let Some(nxt) = neighbor_iter.next() {
+                // Add the neighbor to the new parallel block if ready to be executed and has not been pushed before
+                if self
+                    .dag
+                    .execution_blocked(self.already_executed.as_slice(), &nxt.index())
+                    .is_empty()
+                    && !new_parallel_block.iter().any(|id| *id == nxt.index())
+                {
+                    new_parallel_block.push(nxt.index());
+                }
+            }
+        }
+
+        // Update parallel_block and return it
+        self.parallel_block = new_parallel_block.clone();
+        if new_parallel_block.is_empty() {
+            return None;
+        }
+        Some(new_parallel_block)
     }
 }
 
@@ -692,5 +963,28 @@ mod tests {
         assert!(!dag.last_operation_involving_qubit().is_empty());
 
         assert_eq!(dag.graph.node_count(), op_vec.len());
+    }
+
+    #[test_case(vec![Operation::from(CNOT::new(0,1)), Operation::from(PauliX::new(0)), Operation::from(PauliY::new(0)), Operation::from(PauliZ::new(0))])]
+    #[test_case(vec![Operation::from(PauliZ::new(0)), Operation::from(ControlledPauliZ::new(0,1))])]
+    fn test_from_circuitdag(op_vec: Vec<Operation>) {
+        let mut dag: CircuitDag =
+            CircuitDag::with_capacity(DEFAULT_NODE_NUMBER, DEFAULT_EDGE_NUMBER);
+
+        for op in &op_vec {
+            dag.add_to_back(op.clone());
+        }
+
+        let circuit = Circuit::from(dag.clone());
+
+        assert_eq!(circuit.len(), dag.graph.node_count());
+
+        circuit
+            .iter()
+            .enumerate()
+            .into_iter()
+            .for_each(|(ind, oper)| {
+                assert_eq!(*oper, *dag.graph.node_weight(ind.into()).unwrap());
+            });
     }
 }
