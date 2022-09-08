9298a5a2d541636585bcc36f9bf7b8bdf3063c70
Nicolas Vogt <Nicolas.Vogt@quantumsimulations.de>
2022-09-08 17:21:56 +0200

    Minor fix for beta.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 This changelog track changes to the qoqo project starting at version v0.5.0
 
+## 1.1.0-beta.1
+
+* Allowed creating PragmaSetStateVector from float or integer numpy arrays.
+
 ## 1.1.0-alpha.1
 
 * Added `InputBit` to set bit in a (readout) bit register to a value.
--- qoqo/src/operations/pragma_operations.rs
+++ qoqo/src/operations/pragma_operations.rs
@@ -98,10 +98,26 @@ impl PragmaSetStateVectorWrapper {
     fn new(statevector: Py<PyAny>) -> PyResult<Self> {
         let try_cast: PyResult<Array1<Complex64>> =
             Python::with_gil(|py| -> PyResult<Array1<Complex64>> {
-                let extracted = PyReadonlyArray1::extract(statevector.as_ref(py))?;
+                let extracted: PyReadonlyArray1<Complex64> = statevector.as_ref(py).extract()?;
                 let statevec: Array1<Complex64> = extracted.to_owned_array();
                 Ok(statevec)
             });
+        let try_cast =  try_cast.or_else(|_|
+            Python::with_gil(|py| -> PyResult<Array1<Complex64>> {
+                let extracted: PyReadonlyArray1<f64> = statevector.as_ref(py).extract()?;
+                let statevec: Array1<f64> = extracted.to_owned_array();
+                let statevec: Array1<Complex64> = statevec.into_iter().map(|f| Complex64::new(f, 0.0)).collect();
+                Ok(statevec)
+            })
+        );
+        let try_cast =  try_cast.or_else(|_|
+            Python::with_gil(|py| -> PyResult<Array1<Complex64>> {
+                let extracted: PyReadonlyArray1<isize> = statevector.as_ref(py).extract()?;
+                let statevec: Array1<isize> = extracted.to_owned_array();
+                let statevec: Array1<Complex64> = statevec.into_iter().map(|f| Complex64::new(f as f64, 0.0)).collect();
+                Ok(statevec)
+            })
+        );
         match try_cast {
             Ok(array) => Ok(Self {
                 internal: PragmaSetStateVector::new(array),
