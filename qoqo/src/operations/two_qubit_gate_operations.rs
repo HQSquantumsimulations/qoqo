2af0e3c4ba1096a6bdb2d4a68bbf3d60dbb8b4fa
mlodi-hqs <matteo.lodi@quantumsimulations.de>
2022-10-06 13:33:57 +0000

    doc fix

--- qoqo/src/operations/two_qubit_gate_operations.rs
+++ qoqo/src/operations/two_qubit_gate_operations.rs
@@ -513,7 +513,7 @@ pub struct ComplexPMInteraction {
 ///         1 & 0 & 0 & 0 \\\\
 ///         0 & e^{i \phi} & 0 & 0 \\\\
 ///         0 & 0 & e^{i \phi} & 0 \\\\
-///         0 & 0 & 0 & e^{i (2\cdot\phi - \pi)}
+///         0 & 0 & 0 & e^{i (2\cdot\phi + \pi)}
 ///         \end{pmatrix}
 ///
 /// Args:
