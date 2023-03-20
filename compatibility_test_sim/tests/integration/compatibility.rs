fe2a0e2923af179a74a193bd5a715c9f7822b1db
mlodi-hqs <matteo.lodi@quantumsimulations.de>
2023-03-20 15:41:20 +0000

    no cast problems, wrong assertions

--- compatibility_test_sim/tests/integration/compatibility.rs
+++ compatibility_test_sim/tests/integration/compatibility.rs
@@ -22,21 +22,45 @@ use roqoqo_quest::Backend;
 
 use serde_json;
 use test_case::test_case;
+use num_complex::{Complex, Complex64};
+use ndarray::{array, Array2};
 
 
-#[test_case((false, false, false))]
-#[test_case((false, false, true))]
-#[test_case((false, true, false))]
-#[test_case((false, true, true))]
-#[test_case((true, false, false))]
-#[test_case((true, false, true))]
-#[test_case((true, true, false))]
-#[test_case((true, true, true))]
-fn test_circuits_3q(iteration: (bool, bool, bool)) {
+#[test_case((false, false, false), 0)]
+#[test_case((false, false, true), 1)]
+#[test_case((false, true, false), 2)]
+#[test_case((false, true, true), 3)]
+#[test_case((true, false, false), 4)]
+#[test_case((true, false, true), 5)]
+#[test_case((true, true, false), 6)]
+#[test_case((true, true, true), 7)]
+fn test_circuits_3q(iteration: (bool, bool, bool), value: usize) {
     let ccz = ControlledControlledPauliZ::new(0, 1, 2);
     let ccps = ControlledControlledPhaseShift::new(0, 1, 2, CalculatorFloat::from(0.3));
-    let unitary_ccz = ccz.unitary_matrix();
-    let unitary_ccps = ccps.unitary_matrix();
+    let unitary_ccz = ccz.unitary_matrix().unwrap();
+    let unitary_ccps = ccps.unitary_matrix().unwrap();
+
+    // Preparing matrices for later simulation
+    let c0: Complex64 = Complex::new(0.0, 0.0);
+    let c1: Complex64 = Complex::new(1.0, 0.0);
+    let empty: Array2<Complex64> = array![
+        [c0, c0, c0, c0, c0, c0, c0, c0],
+        [c0, c0, c0, c0, c0, c0, c0, c0],
+        [c0, c0, c0, c0, c0, c0, c0, c0],
+        [c0, c0, c0, c0, c0, c0, c0, c0],
+        [c0, c0, c0, c0, c0, c0, c0, c0],
+        [c0, c0, c0, c0, c0, c0, c0, c0],
+        [c0, c0, c0, c0, c0, c0, c0, c0],
+        [c0, c0, c0, c0, c0, c0, c0, c0],
+    ];
+    let mut final_matrix: Array2<Complex64> = empty.clone();
+    for val in 0..8 {
+        if value == val {
+            final_matrix[[val, val]] = c1;
+        }
+    }
+    let final_matrix_ccz = final_matrix.dot(&unitary_ccz);
+    let final_matrix_ccps = final_matrix.dot(&unitary_ccps);
     
     // Serializing .circuit()
     let ccz_circuit = ccz.circuit();
@@ -68,10 +92,11 @@ fn test_circuits_3q(iteration: (bool, bool, bool)) {
     circuit_ccps_fromj += test_roqoqo_1_2::operations::DefinitionComplex::new("out".to_string(), 3, true);
     circuit_ccps_fromj += test_roqoqo_1_2::operations::PragmaGetStateVector::new("out".to_string(), None);
 
-    // Get results
+    // Get results from circuits()
     let backend = Backend::new(3);
     let (_, _, result_ccz) = backend.run_circuit(&circuit_ccz_fromj).unwrap();
     let (_, _, result_ccps) = backend.run_circuit(&circuit_ccps_fromj).unwrap();
 
-    println!("test");
+    assert_eq!(result_ccz["out"][0], final_matrix_ccz.iter().cloned().collect::<Vec<Complex64>>());
+    assert_eq!(result_ccps["out"][0], final_matrix_ccps.iter().cloned().collect::<Vec<Complex64>>());
 }
