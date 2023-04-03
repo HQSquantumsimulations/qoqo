1c9cb4deb5c5dc7fc0803fa9af21951bd4ad8729
Matteo Lodi <108724576+mlodi-hqs@users.noreply.github.com>
2023-04-03 08:21:32 +0000

    Device support for 3-qubit operations (#472)
    * get-set 3q times roq
    
    * roq tested
    
    * qoqo macro, qoqo tests
    
    * fmt
    
    * macro correction

--- roqoqo/src/devices/all_to_all.rs
+++ roqoqo/src/devices/all_to_all.rs
@@ -174,33 +174,33 @@ impl AllToAllDevice {
             .set_two_qubit_gate_time(gate, control, target, gate_time)
     }
 
-    // /// Setting the gate time of a two qubit gate.
-    // ///
-    // /// # Arguments
-    // ///
-    // /// * `gate` - hqslang name of the two-qubit-gate.
-    // /// * `control_0` - The control_0 qubit for which the gate time is set.
-    // /// * `control_1` - The control_1 qubit for which the gate time is set.
-    // /// * `target` - The target qubit for which the gate time is set.
-    // /// * `gate_time` - gate time for the given gate.
-    // ///
-    // /// # Returns
-    // ///
-    // /// * `Ok(())` - The gate time was correctly set and nothing is returned
-    // /// * `Err(RoqoqoError::GenericError)` - Control_0 qubit set is larger than the number qubits in the device
-    // /// * `Err(RoqoqoError::GenericError)` - Control_1 qubit set is larger than the number qubits in the device
-    // /// * `Err(RoqoqoError::GenericError)` - Target qubit set is larger than the number qubits in the device
-    // pub fn set_three_qubit_gate_time(
-    //     &mut self,
-    //     gate: &str,
-    //     control_0: usize,
-    //     control_1: usize,
-    //     target: usize,
-    //     gate_time: f64,
-    // ) -> Result<(), RoqoqoError> {
-    //     self.generic_device
-    //         .set_three_qubit_gate_time(gate, control_0, control_1, target, gate_time)
-    // }
+    /// Setting the gate time of a two qubit gate.
+    ///
+    /// # Arguments
+    ///
+    /// * `gate` - hqslang name of the two-qubit-gate.
+    /// * `control_0` - The control_0 qubit for which the gate time is set.
+    /// * `control_1` - The control_1 qubit for which the gate time is set.
+    /// * `target` - The target qubit for which the gate time is set.
+    /// * `gate_time` - gate time for the given gate.
+    ///
+    /// # Returns
+    ///
+    /// * `Ok(())` - The gate time was correctly set and nothing is returned
+    /// * `Err(RoqoqoError::GenericError)` - Control_0 qubit set is larger than the number qubits in the device
+    /// * `Err(RoqoqoError::GenericError)` - Control_1 qubit set is larger than the number qubits in the device
+    /// * `Err(RoqoqoError::GenericError)` - Target qubit set is larger than the number qubits in the device
+    pub fn set_three_qubit_gate_time(
+        &mut self,
+        gate: &str,
+        control_0: usize,
+        control_1: usize,
+        target: usize,
+        gate_time: f64,
+    ) -> Result<(), RoqoqoError> {
+        self.generic_device
+            .set_three_qubit_gate_time(gate, control_0, control_1, target, gate_time)
+    }
 
     /// Setting the gate time of a mulit qubit gate.
     ///
@@ -396,16 +396,16 @@ impl Device for AllToAllDevice {
             .two_qubit_gate_time(hqslang, control, target)
     }
 
-    // fn three_qubit_gate_time(
-    //     &self,
-    //     hqslang: &str,
-    //     control_0: &usize,
-    //     control_1: &usize,
-    //     target: &usize,
-    // ) -> Option<f64> {
-    //     self.generic_device
-    //         .three_qubit_gate_time(hqslang, control_0, control_1, target)
-    // }
+    fn three_qubit_gate_time(
+        &self,
+        hqslang: &str,
+        control_0: &usize,
+        control_1: &usize,
+        target: &usize,
+    ) -> Option<f64> {
+        self.generic_device
+            .three_qubit_gate_time(hqslang, control_0, control_1, target)
+    }
 
     fn multi_qubit_gate_time(&self, hqslang: &str, qubits: &[usize]) -> Option<f64> {
         self.generic_device.multi_qubit_gate_time(hqslang, qubits)
