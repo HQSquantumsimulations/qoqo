56acefc9a1aad34da8510ce855dbf3b7b7b5f668
Kirsten Bark <kirsten.bark@quantumsimulations.de>
2023-06-27 15:52:15 +0200

    Fixed copy-paste error

--- roqoqo/src/operations/bosonic_operations.rs
+++ roqoqo/src/operations/bosonic_operations.rs
@@ -22,7 +22,7 @@ use qoqo_calculator::CalculatorFloat;
 
 /// The single-mode squeezing gate with tunable squeezing.
 ///
-/// The squeezing gate is a quantum operation that reduces that allows for precise manipulation of quantum states,
+/// The squeezing gate is a quantum operation that allows for precise manipulation of quantum states,
 /// by reducing the uncertainty in one variable and therefore increasing the uncertainty of another.
 /// https://arxiv.org/pdf/quant-ph/0106157.pdf
 ///
