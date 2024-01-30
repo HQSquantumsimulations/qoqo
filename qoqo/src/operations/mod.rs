5454baf0c186aa9567545999f69e9c2be0df7e74
Divyanshu <divyanshu.gupta@quantumsimulations.de>
2024-01-30 13:36:43 +0100

    added time dependent spin hamiltonian

--- qoqo/src/operations/mod.rs
+++ qoqo/src/operations/mod.rs
@@ -164,5 +164,6 @@ pub fn operations(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<JaynesCummingsWrapper>()?;
 
     m.add_class::<ApplyConstantSpinHamiltonianWrapper>()?;
+    m.add_class::<ApplyTimeDependentSpinHamiltonianWrapper>()?;
     Ok(())
 }
