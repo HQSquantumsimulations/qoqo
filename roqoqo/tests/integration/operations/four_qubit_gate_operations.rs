51d9814d30bc80087127a0ae75796b34bf61befe
mlodi-hqs <matteo.lodi@quantumsimulations.de>
2024-09-16 14:27:44 +0200

    tests

--- roqoqo/tests/integration/operations/four_qubit_gate_operations.rs
+++ roqoqo/tests/integration/operations/four_qubit_gate_operations.rs
@@ -16,7 +16,31 @@ use ndarray::array;
 use num_complex::Complex64;
 use qoqo_calculator::Calculator;
 use roqoqo::operations::*;
+use roqoqo::Circuit;
 use std::collections::{HashMap, HashSet};
+
+#[test]
+fn test_circuit_triple_controlled_x() {
+    let gate = TripleControlledPauliX::new(0, 1, 2, 3);
+    let c = gate.circuit();
+
+    let mut comparison_circuit = Circuit::new();
+    comparison_circuit += CNOT::new(0, 3);
+    comparison_circuit += CNOT::new(0, 1);
+    comparison_circuit += CNOT::new(1, 3);
+    comparison_circuit += CNOT::new(0, 1);
+    comparison_circuit += CNOT::new(1, 3);
+    comparison_circuit += CNOT::new(1, 2);
+    comparison_circuit += CNOT::new(2, 3);
+    comparison_circuit += CNOT::new(0, 2);
+    comparison_circuit += CNOT::new(2, 3);
+    comparison_circuit += CNOT::new(1, 2);
+    comparison_circuit += CNOT::new(2, 3);
+    comparison_circuit += CNOT::new(0, 2);
+    comparison_circuit += CNOT::new(2, 3);
+
+    assert!(c == comparison_circuit);
+}
 
 #[test]
 fn test_matrix_output_triple_controlled_x() {
@@ -336,7 +360,7 @@ fn test_operate_triple_controlled_x() {
         &[
             "Operation",
             "GateOperation",
-            "MultiQubitGateOperation",
+            "FourQubitGateOperation",
             "TripleControlledPauliX",
         ]
     );
@@ -365,16 +389,11 @@ fn test_substitute_triple_controlled_x() {
 #[test]
 fn test_substitute_error_triple_controlled_x() {
     let gate = TripleControlledPauliX::new(0, 1, 2, 3);
-    let mut substitution_dict: Calculator = Calculator::new();
-    substitution_dict.set_variable("error", 0.0);
-    let result = gate.substitute_parameters(&substitution_dict);
-    assert!(result.is_err());
-
     let mut mapping: HashMap<usize, usize> = std::collections::HashMap::new();
     let _ = mapping.insert(1, 2);
     let _ = mapping.insert(2, 3);
     let _ = mapping.insert(3, 4);
-    let _ = mapping.insert(4, 1);
+    let _ = mapping.insert(4, 0);
     let remapped = gate.remap_qubits(&mapping);
     assert!(remapped.is_err());
 }
@@ -394,6 +413,409 @@ fn test_format_triple_controlled_x() {
 #[test]
 fn test_involved_qubits_triple_controlled_x() {
     let gate = TripleControlledPauliX::new(0, 1, 2, 3);
+    let involved_qubits = gate.involved_qubits();
+    let mut comp_set: HashSet<usize> = HashSet::new();
+    let _ = comp_set.insert(0);
+    let _ = comp_set.insert(1);
+    let _ = comp_set.insert(2);
+    let _ = comp_set.insert(3);
+    assert_eq!(involved_qubits, InvolvedQubits::Set(comp_set));
+}
+
+#[test]
+fn test_circuit_triple_controlled_z() {
+    let gate = TripleControlledPauliZ::new(0, 1, 2, 3);
+    let c = gate.circuit();
+
+    let mut comparison_circuit = Circuit::new();
+    comparison_circuit += ControlledPauliZ::new(0, 3);
+    comparison_circuit += CNOT::new(0, 1);
+    comparison_circuit += ControlledPauliZ::new(1, 3);
+    comparison_circuit += CNOT::new(0, 1);
+    comparison_circuit += ControlledPauliZ::new(1, 3);
+    comparison_circuit += CNOT::new(1, 2);
+    comparison_circuit += ControlledPauliZ::new(2, 3);
+    comparison_circuit += CNOT::new(0, 2);
+    comparison_circuit += ControlledPauliZ::new(2, 3);
+    comparison_circuit += CNOT::new(1, 2);
+    comparison_circuit += ControlledPauliZ::new(2, 3);
+    comparison_circuit += CNOT::new(0, 2);
+    comparison_circuit += ControlledPauliZ::new(2, 3);
+
+    assert!(c == comparison_circuit);
+}
+
+#[test]
+fn test_matrix_output_triple_controlled_z() {
+    let gate = TripleControlledPauliZ::new(0, 1, 2, 3);
+    let test_array = array![
+        [
+            Complex64::new(1.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+        ],
+        [
+            Complex64::new(0.0, 0.0),
+            Complex64::new(1.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+        ],
+        [
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(1.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+        ],
+        [
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(1.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+        ],
+        [
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(1.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+        ],
+        [
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(1.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+        ],
+        [
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(1.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+        ],
+        [
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(1.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+        ],
+        [
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(1.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+        ],
+        [
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(1.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+        ],
+        [
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(1.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+        ],
+        [
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(1.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+        ],
+        [
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(1.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+        ],
+        [
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(1.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+        ],
+        [
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(1.0, 0.0),
+            Complex64::new(0.0, 0.0),
+        ],
+        [
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(-1.0, 0.0),
+        ],
+    ];
+    let unit = gate.unitary_matrix().unwrap();
+    let should_be_zero = unit - test_array;
+    assert!(should_be_zero.iter().all(|x| x.norm() < f64::EPSILON));
+}
+
+#[test]
+fn test_clone_partial_eq_triple_controlled_z() {
+    let gate = TripleControlledPauliZ::new(0, 1, 2, 3);
+    let gate1 = TripleControlledPauliZ::new(1, 2, 3, 4);
+    let helper = gate != gate1;
+    assert!(helper);
+    #[allow(clippy::redundant_clone)]
+    let gate2 = gate1.clone();
+    assert_eq!(gate2, gate1);
+}
+
+#[test]
+fn test_operate_triple_controlled_z() {
+    let gate = TripleControlledPauliZ::new(0, 1, 2, 3);
+    assert_eq!(gate.hqslang(), "TripleControlledPauliZ");
+    assert_eq!(
+        gate.tags(),
+        &[
+            "Operation",
+            "GateOperation",
+            "FourQubitGateOperation",
+            "TripleControlledPauliZ",
+        ]
+    );
+    assert!(!gate.is_parametrized());
+}
+
+#[test]
+fn test_substitute_triple_controlled_z() {
+    let gate = TripleControlledPauliZ::new(0, 1, 2, 3);
+    let gate1 = TripleControlledPauliZ::new(1, 2, 3, 0);
+    let mut substitution_dict: Calculator = Calculator::new();
+    substitution_dict.set_variable("theta", 0.0);
+    let result = gate.substitute_parameters(&substitution_dict).unwrap();
+    assert_eq!(result, gate);
+
+    let mut mapping: HashMap<usize, usize> = std::collections::HashMap::new();
+    let _ = mapping.insert(0, 1);
+    let _ = mapping.insert(1, 2);
+    let _ = mapping.insert(2, 3);
+    let _ = mapping.insert(3, 0);
+    let remapped = gate1.remap_qubits(&mapping).unwrap();
+    let qubits = remapped.involved_qubits();
+    assert_eq!(qubits, InvolvedQubits::Set(HashSet::from([1, 2, 3, 0])));
+}
+
+#[test]
+fn test_substitute_error_triple_controlled_z() {
+    let gate = TripleControlledPauliX::new(0, 1, 2, 3);
+    let mut mapping: HashMap<usize, usize> = std::collections::HashMap::new();
+    let _ = mapping.insert(1, 2);
+    let _ = mapping.insert(2, 3);
+    let _ = mapping.insert(3, 4);
+    let _ = mapping.insert(4, 0);
+    let remapped = gate.remap_qubits(&mapping);
+    assert!(remapped.is_err());
+}
+
+#[test]
+fn test_format_triple_controlled_z() {
+    let gate = TripleControlledPauliZ::new(0, 1, 2, 3);
+    let string = format!("{:?}", gate);
+    assert!(string.contains("TripleControlledPauliZ"));
+    assert!(string.contains("control_0"));
+    assert!(string.contains("control_1"));
+    assert!(string.contains("control_2"));
+    assert!(string.contains("target"));
+    println!("{:?}", string);
+}
+
+#[test]
+fn test_involved_qubits_triple_controlled_z() {
+    let gate = TripleControlledPauliZ::new(0, 1, 2, 3);
     let involved_qubits = gate.involved_qubits();
     let mut comp_set: HashSet<usize> = HashSet::new();
     let _ = comp_set.insert(0);
