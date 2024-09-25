207ef14f6211438200bafbbaeedd65e8d515786a
mlodi-hqs <matteo.lodi@quantumsimulations.de>
2024-09-25 14:01:30 +0200

    prelude

--- roqoqo/src/prelude.rs
+++ roqoqo/src/prelude.rs
@@ -31,5 +31,6 @@ pub use crate::operations::{
     OperateSingleModeGate, OperateSingleQubit, OperateSingleQubitGate, OperateThreeQubit,
     OperateThreeQubitGate, OperateTwoMode, OperateTwoModeGate, OperateTwoQubit,
     OperateTwoQubitGate, Rotate, Substitute, SubstituteModes, SupportedVersion,
+    AVAILABLE_GATES_NAMES,
 };
 pub use crate::{RoqoqoBackendError, RoqoqoError};
