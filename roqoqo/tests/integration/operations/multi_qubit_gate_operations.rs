c8ede782ff5e131aa09ad4960a94628fbffe0fc3
Kirsten Bark <kirsten.bark@quantumsimulations.de>
2025-07-08 11:56:48 +0200

    Added QFT operation

--- roqoqo/tests/integration/operations/multi_qubit_gate_operations.rs
+++ roqoqo/tests/integration/operations/multi_qubit_gate_operations.rs
@@ -23,7 +23,10 @@ use roqoqo::operations::*;
 use roqoqo::Circuit;
 #[cfg(feature = "json_schema")]
 use schemars::schema_for;
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    f64::consts::{FRAC_PI_2, FRAC_PI_4, PI},
+};
 use test_case::test_case;
 
 /// Test circuit function of MultiQubitMolmerSorensen
@@ -678,12 +681,14 @@ fn test_rotatex_powercf_multi_qubit_zz(theta: CalculatorFloat, power: Calculator
 #[test_case(MultiQubitGateOperation::from(MultiQubitZZ::new(vec![0, 1, 2, 3], 0.23.into())); "MultiQubitZZ")]
 #[test_case(MultiQubitGateOperation::from(MultiQubitMS::new(vec![0, 1, 2], 0.45.into())); "MultiQubitMS")]
 #[test_case(MultiQubitGateOperation::from(MultiQubitCNOT::new(vec![0, 1, 2])); "MultiQubitCNOT")]
+#[test_case(MultiQubitGateOperation::from(QFT::new(vec![0, 1, 2], true, false)); "QFT")]
 pub fn test_json_schema_multi_qubit_gate_operations(gate: MultiQubitGateOperation) {
     // Serialize
     let test_json = match gate.clone() {
         MultiQubitGateOperation::MultiQubitMS(op) => serde_json::to_string(&op).unwrap(),
         MultiQubitGateOperation::MultiQubitZZ(op) => serde_json::to_string(&op).unwrap(),
         MultiQubitGateOperation::MultiQubitCNOT(op) => serde_json::to_string(&op).unwrap(),
+        MultiQubitGateOperation::QFT(op) => serde_json::to_string(&op).unwrap(),
         _ => unreachable!(),
     };
     let test_value: serde_json::Value = serde_json::from_str(&test_json).unwrap();
@@ -693,6 +698,7 @@ pub fn test_json_schema_multi_qubit_gate_operations(gate: MultiQubitGateOperatio
         MultiQubitGateOperation::MultiQubitMS(_) => schema_for!(MultiQubitMS),
         MultiQubitGateOperation::MultiQubitZZ(_) => schema_for!(MultiQubitZZ),
         MultiQubitGateOperation::MultiQubitCNOT(_) => schema_for!(MultiQubitCNOT),
+        MultiQubitGateOperation::QFT(_) => schema_for!(QFT),
         _ => unreachable!(),
     };
     let schema = serde_json::to_string(&test_schema).unwrap();
@@ -858,4 +864,171 @@ pub fn test_json_schema_multi_qubit_call_gate() {
 
     let validation_result = compiled_schema.validate(&test_value);
     assert!(validation_result.is_ok());
+}
+
+/// Test circuit function of QFT
+#[test_case(vec![0], false, false; "one_qubit")]
+#[test_case(vec![0], true, false; "one_qubit_swap")]
+#[test_case(vec![0], false, true; "one_qubit_inv")]
+#[test_case(vec![0], true, true; "one_qubit_swap_inv")]
+#[test_case(vec![0,1], false, false; "two_qubit")]
+#[test_case(vec![0,1], true, false; "two_qubit_swap")]
+#[test_case(vec![0,1], false, true; "two_qubit_inv")]
+#[test_case(vec![0,1], true, true; "two_qubit_swap_inv")]
+#[test_case(vec![0,1,2], false, false; "three_qubit")]
+#[test_case(vec![0,1,2], true, false; "three_qubit_swap")]
+#[test_case(vec![0,1,2], false, true; "three_qubit_inv")]
+#[test_case(vec![0,1,2], true, true; "three_qubit_swap_inv")]
+fn test_circuit_qft(qubits: Vec<usize>, swap: bool, inverse: bool) {
+    let gate = QFT::new(qubits.clone(), swap, inverse);
+    let c = gate.circuit();
+    let sign = if inverse { -1. } else { 1. };
+    if qubits.len() == 1 {
+        let mut comparison_circuit = Circuit::new();
+        comparison_circuit += Hadamard::new(0);
+        assert!(c == comparison_circuit);
+    } else if qubits.len() == 2 {
+        let mut comparison_circuit = Circuit::new();
+        if swap && inverse {
+            comparison_circuit += SWAP::new(0, 1);
+        }
+        comparison_circuit += Hadamard::new(0);
+        comparison_circuit += ControlledPhaseShift::new(1, 0, (sign * FRAC_PI_2).into());
+        comparison_circuit += Hadamard::new(1);
+        if swap && !inverse {
+            comparison_circuit += SWAP::new(0, 1);
+        }
+        assert!(c == comparison_circuit);
+    } else if qubits.len() == 3 {
+        let mut comparison_circuit = Circuit::new();
+        if swap && inverse {
+            comparison_circuit += SWAP::new(0, 2);
+        }
+        comparison_circuit += Hadamard::new(0);
+        comparison_circuit += ControlledPhaseShift::new(1, 0, (sign * FRAC_PI_2).into());
+        comparison_circuit += ControlledPhaseShift::new(2, 0, (sign * FRAC_PI_4).into());
+        comparison_circuit += Hadamard::new(1);
+        comparison_circuit += ControlledPhaseShift::new(2, 1, (sign * FRAC_PI_2).into());
+        comparison_circuit += Hadamard::new(2);
+        if swap && !inverse {
+            comparison_circuit += SWAP::new(0, 2);
+        }
+        assert!(c == comparison_circuit);
+    }
+}
+
+#[test_case(vec![0], false; "one_qubit")]
+#[test_case(vec![0], true; "one_qubit_inv")]
+fn test_matrix_output_qft(qubits: Vec<usize>, inverse: bool) {
+    let dim = qubits.len();
+    let r = 1. / 2_f64.powi(dim as i32).sqrt();
+    let gate = QFT::new(qubits.clone(), true, inverse);
+    let mut test_array = match qubits.len() {
+        1 => array![
+            [Complex64::from_polar(r, 0.), Complex64::from_polar(r, 0.)],
+            [Complex64::from_polar(r, 0.), Complex64::from_polar(r, PI)],
+        ],
+        2 => array![
+            [
+                Complex64::from_polar(r, 0.),
+                Complex64::from_polar(r, 0.),
+                Complex64::from_polar(r, 0.),
+                Complex64::from_polar(r, 0.)
+            ],
+            [
+                Complex64::from_polar(r, 0.),
+                Complex64::from_polar(r, FRAC_PI_2),
+                Complex64::from_polar(r, PI),
+                Complex64::from_polar(r, 3. * FRAC_PI_2)
+            ],
+            [
+                Complex64::from_polar(r, 0.),
+                Complex64::from_polar(r, PI),
+                Complex64::from_polar(r, 0.),
+                Complex64::from_polar(r, PI)
+            ],
+            [
+                Complex64::from_polar(r, 0.),
+                Complex64::from_polar(r, 3. * FRAC_PI_2),
+                Complex64::from_polar(r, PI),
+                Complex64::from_polar(r, FRAC_PI_2)
+            ],
+        ],
+        _ => unreachable!(),
+    };
+    if inverse {
+        test_array.iter_mut().for_each(|x| *x = x.conj());
+    }
+    let unit = gate.unitary_matrix().unwrap();
+    println!("{:.2}", test_array);
+    println!("{:.2}", unit);
+    let should_be_zero = unit - test_array;
+    assert!(should_be_zero.iter().all(|x| x.norm() < f64::EPSILON));
+}
+
+#[test]
+fn test_clone_partial_eq_qft() {
+    let qubits = vec![0, 1, 2];
+
+    let gate = QFT::new(qubits.clone(), true, true);
+    assert_eq!(gate.hqslang(), "QFT");
+    assert_eq!(
+        gate.tags(),
+        &[
+            "Operation",
+            "GateOperation",
+            "MultiQubitGateOperation",
+            "QFT",
+        ]
+    );
+    assert!(!gate.is_parametrized());
+    let gate2 = gate.clone();
+    assert_eq!(gate2, gate);
+}
+
+#[test]
+fn test_substitute_qft() {
+    let qubits = vec![0, 1, 2];
+    let gate = QFT::new(qubits.clone(), false, false);
+    let mut mapping: HashMap<usize, usize> = std::collections::HashMap::new();
+    let _ = mapping.insert(0, 1);
+    let _ = mapping.insert(1, 2);
+    let _ = mapping.insert(2, 0);
+    let remapped = gate.remap_qubits(&mapping).unwrap();
+    let qubits = remapped.qubits();
+    assert_eq!(qubits, &vec![1, 2, 0]);
+}
+
+#[test]
+fn test_substitute_error_qft() {
+    let qubits = vec![0, 1, 2];
+    let gate = QFT::new(qubits.clone(), false, false);
+    let mut mapping: HashMap<usize, usize> = std::collections::HashMap::new();
+    let _ = mapping.insert(1, 2);
+    let _ = mapping.insert(2, 0);
+    let remapped = gate.remap_qubits(&mapping);
+    assert!(remapped.is_err());
+}
+
+#[test]
+fn test_format_error_qft() {
+    let qubits = vec![0, 1, 2];
+    let gate = QFT::new(qubits.clone(), false, false);
+    let string = format!("{:?}", gate);
+    assert!(string.contains("QFT"));
+}
+
+#[test_case(false, false; "qft")]
+#[test_case(true, false; "qft_swap")]
+#[test_case(false, true; "qft_inv")]
+#[test_case(false, false; "qft_swap_inv")]
+fn test_involved_qubits_qft(swaps: bool, inverse: bool) {
+    let qubits = vec![0, 1, 2];
+    let gate = QFT::new(qubits.clone(), swaps, inverse);
+    let involved_qubits = gate.involved_qubits();
+    let mut comp_set: HashSet<usize> = HashSet::new();
+    let _ = comp_set.insert(0);
+    let _ = comp_set.insert(1);
+    let _ = comp_set.insert(2);
+    assert_eq!(involved_qubits, InvolvedQubits::Set(comp_set));
 }
