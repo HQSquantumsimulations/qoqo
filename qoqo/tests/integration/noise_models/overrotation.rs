577ebc2900e84484f89af3fff5cf9cc4891e2376
kbarkhqs <65908581+kbarkhqs@users.noreply.github.com>
2025-12-10 16:05:02 +0100

    Removed `unstable_qoqo_devices` (#719)
    * Added documentation links
    
    * Trying to fix compatibility tests
    
    * Bumped version to 1.20.1
    
    * Got it to run without tildes
    
    * Removed unnecessary dep
    
    * added restrictions in pyproject.toml
    
    * Added tildes back in
    
    * Fixing compatibility tests
    
    * Updated MSPV
    
    * Removed unstable_analog_operations
    
    * Updated changelog and deps
    
    * Removed unstable_simulation_repetitions
    
    * Fixed clippy
    
    * Removed comments
    
    * Removed comments
    
    * Removed comments
    
    * Clippy in tests
    
    * Removed unstable_qoqo_devices

--- qoqo/tests/integration/noise_models/overrotation.rs
+++ qoqo/tests/integration/noise_models/overrotation.rs
@@ -229,7 +229,7 @@ fn test_to_from_bincode() {
 
         let deserialised_error = new_br.call_method1(
             "from_bincode",
-            (bincode::serde::encode_to_vec(&vec![0], bincode::config::legacy()).unwrap(),),
+            (bincode::serde::encode_to_vec(vec![0], bincode::config::legacy()).unwrap(),),
         );
         assert!(deserialised_error.is_err());
 
@@ -266,7 +266,7 @@ fn test_to_from_bincode_description() {
 
         let deserialised_error = new_br.call_method1(
             "from_bincode",
-            (bincode::serde::encode_to_vec(&vec![0], bincode::config::legacy()).unwrap(),),
+            (bincode::serde::encode_to_vec(vec![0], bincode::config::legacy()).unwrap(),),
         );
         assert!(deserialised_error.is_err());
 
